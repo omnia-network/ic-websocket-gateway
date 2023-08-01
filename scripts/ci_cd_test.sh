d868704ff91d2130ccc6ae68070038f27f63c268
Luca8991 <luca.berte98@hotmail.it>
2023-08-01 14:58:13 +0200

    chore: github action for tests

---
+++ scripts/ci_cd_test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+echo "Starting local replica"
+dfx start --clean --background
+
+echo "Running unit tests for gateway"
+cargo test -- --test-threads=1
+
+echo "Starting gateway in the background"
+cargo run > /dev/null 2>&1 &
+gateway_pid=$!
+
+GATEWAY_PRINCIPAL=$(cargo run -q -p scripts --bin principal_from_key_pair "./data/key_pair")
+
+echo "Deploying test canister"
+cd tests/test_canister
+dfx deploy test_canister --no-wallet --argument "(opt \"$GATEWAY_PRINCIPAL\")"
+
+# source the canister id from the generated .env file
+source .env
+
+cd ../integration
+
+echo "Running integration test"
+WS_GATEWAY_URL=ws://127.0.0.1:8080 IC_URL=http://127.0.0.1:4943 FETCH_IC_ROOT_KEY=true DFX_NETWORK=local TEST_CANISTER_ID=$CANISTER_ID_TEST_CANISTER npm test
+
+echo "Stopping gateway"
+kill $gateway_pid
+
+echo "Stopping local replica"
+dfx stop
