e2ee0424d1828b97a6b3b644a1b08ae49bc04f17
Luca8991 <luca.berte98@hotmail.it>
2023-09-20 19:30:01 +0200

    chore: put the https cert in the right path

--- scripts/certbot_certonly.sh
+++ scripts/certbot_certonly.sh
@@ -3,4 +3,4 @@
 source .env
 
 # obtain or renew a certificate by spinning up a temporary webserver on port 80
-docker run -it --rm --name certbot -v $(pwd)/volumes/nginx/certs:/etc/letsencrypt -p 80:80 certbot/certbot certonly -d $DOMAIN_NAME --standalone
+docker run -it --rm --name certbot -v $(pwd)/volumes/ic-ws-gateway/data/certs:/etc/letsencrypt -p 80:80 certbot/certbot certonly -d $DOMAIN_NAME --standalone
