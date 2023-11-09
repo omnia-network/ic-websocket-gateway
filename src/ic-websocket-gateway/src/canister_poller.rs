4956a7ea4d181cd145f21e198105e2e5d3072d0d
massimoalbarello <massimo00albarello@gmail.com>
2023-11-09 11:19:38 +0100

    renamed get_messages_operation

--- src/ic-websocket-gateway/src/canister_poller.rs
+++ src/ic-websocket-gateway/src/canister_poller.rs
@@ -129,10 +129,10 @@ impl CanisterPoller {
         // this is necessary as the poller once it starts it does not know the nonce of the last message delivered by the canister
         messages_demux.add_client_channel(first_client_key.clone(), message_for_client_tx);
 
-        let get_messages_operation = self.get_canister_updates(first_client_key.clone());
-        // pin the tracking of the in-flight asynchronous operation so that in each select! iteration get_messages_operation is continued
+        let get_canister_updates = self.get_canister_updates(first_client_key.clone());
+        // pin the tracking of the in-flight asynchronous operation so that in each select! iteration get_canister_updates is continued
         // instead of issuing a new call to get_canister_updates
-        tokio::pin!(get_messages_operation);
+        tokio::pin!(get_canister_updates);
 
         'poller_loop: loop {
             select! {
@@ -159,7 +159,7 @@ impl CanisterPoller {
                     }
                 }
                 // poll canister for updates across multiple select! iterations
-                res = &mut get_messages_operation => {
+                res = &mut get_canister_updates => {
                     // process messages in queues before the ones just polled from the canister (if any) so that the clients receive messages in the expected order
                     // this is done even if no messages are returned from the current polling iteration as there might be messages in the queue waiting to be processed
                     messages_demux.process_queues().await;
@@ -197,7 +197,7 @@ impl CanisterPoller {
                     *self.polling_iteration.write().await += 1;
 
                     // pin a new asynchronous operation so that it can be restarted in the next select! iteration and continued in the following ones
-                    get_messages_operation.set(self.get_canister_updates(first_client_key.clone()));
+                    get_canister_updates.set(self.get_canister_updates(first_client_key.clone()));
                 },
             }
             // exit task if last client disconnected
