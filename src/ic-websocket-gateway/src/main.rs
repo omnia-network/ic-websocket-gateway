1b1799f95a773c8e75a0ec55133877d8fd1cd733
massimoalbarello <massimo00albarello@gmail.com>
2023-11-14 16:25:27 +0100

    wip: admin server for dynamic tracing filtering

--- src/ic-websocket-gateway/src/main.rs
+++ src/ic-websocket-gateway/src/main.rs
@@ -1,16 +1,30 @@
 use crate::ws_listener::TlsConfig;
 use crate::{events_analyzer::EventsAnalyzer, gateway_server::GatewayServer};
+use bytes::Bytes;
+use futures::{
+    future::{self, Ready},
+    Future,
+};
+use http::{Method, Request, Response, StatusCode};
+use hyper::{server::conn::AddrStream, Body, Server};
 use ic_identity::{get_identity_from_key_pair, load_key_pair};
+use std::error::Error;
 use std::{
     fs::{self, File},
     path::Path,
     time::{SystemTime, UNIX_EPOCH},
 };
+use std::{
+    net::SocketAddr,
+    pin::Pin,
+    task::{Context, Poll},
+};
 use structopt::StructOpt;
 use tokio::sync::mpsc::{self, Receiver, Sender};
-use tracing::info;
+use tower::{Service, ServiceBuilder};
+use tracing::{self, error, info, trace};
 use tracing_appender::non_blocking::WorkerGuard;
-use tracing_subscriber::{prelude::*, EnvFilter};
+use tracing_subscriber::{filter::EnvFilter, reload::Handle};
 
 mod canister_methods;
 mod canister_poller;
@@ -62,6 +76,97 @@ struct DeploymentInfo {
     tls_certificate_key_pem_path: Option<String>,
 }
 
+type Err = Box<dyn Error + Send + Sync + 'static>;
+
+struct AdminSvc<S> {
+    handle: Handle<EnvFilter, S>,
+}
+
+impl<S> Clone for AdminSvc<S> {
+    fn clone(&self) -> Self {
+        Self {
+            handle: self.handle.clone(),
+        }
+    }
+}
+
+impl<'a, S> Service<&'a AddrStream> for AdminSvc<S>
+where
+    S: tracing::Subscriber,
+{
+    type Response = AdminSvc<S>;
+    type Error = hyper::Error;
+    type Future = Ready<Result<Self::Response, Self::Error>>;
+
+    fn poll_ready(&mut self, _: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, _: &'a AddrStream) -> Self::Future {
+        future::ok(self.clone())
+    }
+}
+
+impl<S> Service<Request<Body>> for AdminSvc<S>
+where
+    S: tracing::Subscriber + 'static,
+{
+    type Response = Response<Body>;
+    type Error = Err;
+    type Future = Pin<Box<dyn Future<Output = Result<Response<Body>, Err>> + std::marker::Send>>;
+
+    fn poll_ready(&mut self, _: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, req: Request<Body>) -> Self::Future {
+        // we need to clone so that the reference to self
+        // isn't outlived by the returned future.
+        let handle = self.clone();
+        let f = async move {
+            let rsp = match (req.method(), req.uri().path()) {
+                (&Method::PUT, "/filter") => {
+                    trace!("setting filter");
+
+                    let body = hyper::body::to_bytes(req).await?;
+                    match handle.set_from(body) {
+                        Err(error) => {
+                            error!(%error, "setting filter failed!");
+                            rsp(StatusCode::INTERNAL_SERVER_ERROR, error)
+                        },
+                        Ok(()) => rsp(StatusCode::NO_CONTENT, Body::empty()),
+                    }
+                },
+                _ => rsp(StatusCode::NOT_FOUND, "try `/filter`"),
+            };
+            Ok(rsp)
+        };
+        Box::pin(f)
+    }
+}
+
+impl<S> AdminSvc<S>
+where
+    S: tracing::Subscriber + 'static,
+{
+    fn set_from(&self, bytes: Bytes) -> Result<(), String> {
+        use std::str;
+        let body = str::from_utf8(bytes.as_ref()).map_err(|e| format!("{}", e))?;
+        trace!(request.body = ?body);
+        let new_filter = body
+            .parse::<tracing_subscriber::filter::EnvFilter>()
+            .map_err(|e| format!("{}", e))?;
+        self.handle.reload(new_filter).map_err(|e| format!("{}", e))
+    }
+}
+
+fn rsp(status: StatusCode, body: impl Into<Body>) -> Response<Body> {
+    Response::builder()
+        .status(status)
+        .body(body.into())
+        .expect("builder with known status code must not fail")
+}
+
 fn create_data_dir() -> Result<(), String> {
     if !Path::new("./data").is_dir() {
         fs::create_dir("./data").map_err(|e| e.to_string())?;
@@ -69,7 +174,7 @@ fn create_data_dir() -> Result<(), String> {
     Ok(())
 }
 
-fn init_tracing() -> Result<(WorkerGuard, WorkerGuard), String> {
+fn init_tracing(gateway_address: &str) -> Result<(WorkerGuard, WorkerGuard), String> {
     if !Path::new("./data/traces").is_dir() {
         fs::create_dir("./data/traces").map_err(|e| e.to_string())?;
     }
@@ -82,44 +187,61 @@ fn init_tracing() -> Result<(WorkerGuard, WorkerGuard), String> {
     println!("Tracing to file: {}", filename);
 
     let log_file = File::create(filename).map_err(|e| e.to_string())?;
-    let (non_blocking_file, guard_file) = tracing_appender::non_blocking(log_file);
+    let (_non_blocking_file, guard_file) = tracing_appender::non_blocking(log_file);
     let (non_blocking_stdout, guard_stdout) = tracing_appender::non_blocking(std::io::stdout());
 
-    let env_filter_file = EnvFilter::builder()
-        .with_env_var("RUST_LOG_FILE")
-        .try_from_env()
-        .unwrap_or_else(|_| EnvFilter::new("ic_websocket_gateway=trace"));
-
-    let file_tracing_layer = tracing_subscriber::fmt::layer()
-        .json()
-        .with_writer(non_blocking_file)
-        .with_thread_ids(true)
-        .with_filter(env_filter_file);
+    // let env_filter_file = EnvFilter::builder()
+    //     .with_env_var("RUST_LOG_FILE")
+    //     .try_from_env()
+    //     .unwrap_or_else(|_| EnvFilter::new("ic_websocket_gateway=trace"));
+    // let file_tracing_layer = tracing_subscriber::fmt::layer()
+    //     .json()
+    //     .with_writer(non_blocking_file)
+    //     .with_thread_ids(true)
+    //     .with_filter(env_filter_file);
 
     let env_filter_stdout = EnvFilter::builder()
         .with_env_var("RUST_LOG_STDOUT")
         .try_from_env()
         .unwrap_or_else(|_| EnvFilter::new("ic_websocket_gateway=info"));
-    let stdout_tracing_layer = tracing_subscriber::fmt::layer()
+    let stdout_tracing_layer = tracing_subscriber::fmt()
         .with_writer(non_blocking_stdout)
         .pretty()
-        .with_filter(env_filter_stdout);
+        .with_env_filter(env_filter_stdout)
+        // it is possible to dinamically reconfigure the filter with the following command:
+        // :; curl -X PUT localhost:3001/filter -d "ic_websocket_gateway=error"
+        //
+        .with_filter_reloading();
 
-    tracing_subscriber::registry()
-        .with(file_tracing_layer)
-        .with(stdout_tracing_layer)
-        .init();
+    let handle = stdout_tracing_layer.reload_handle();
+    stdout_tracing_layer.init();
+
+    let admin_addr = gateway_address[..gateway_address.len() - 5].to_string() + ":3001";
+    let admin_addr = admin_addr
+        .parse::<SocketAddr>()
+        .map_err(|e| e.to_string())?;
+    let admin = ServiceBuilder::new().service(AdminSvc { handle });
+    let admin = Server::bind(&admin_addr).serve(admin);
+    info!(
+        "Admin server for dynamic tracing filter configuration listening on {}",
+        admin_addr
+    );
+
+    tokio::spawn(admin);
+    // tracing_subscriber::registry()
+    //     .with(file_tracing_layer)
+    //     .init();
 
     Ok((guard_file, guard_stdout))
 }
 
 #[tokio::main]
 async fn main() -> Result<(), String> {
-    create_data_dir()?;
-    let _guards = init_tracing().expect("could not init tracing");
-
     let deployment_info = DeploymentInfo::from_args();
     info!("Deployment info: {:?}", deployment_info);
+
+    create_data_dir()?;
+    let _guards = init_tracing(&deployment_info.gateway_address).expect("could not init tracing");
 
     let key_pair = load_key_pair("./data/key_pair")?;
     let identity = get_identity_from_key_pair(key_pair);
