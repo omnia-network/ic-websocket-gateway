f7f9dc1dcffddc1f3e33a683e8704e1f6be45e13
Lorenzo Ronzani <lorenzo.ronzani00@gmail.com>
2024-03-12 14:38:25 +0100

    feat 🦄:  Developed a custom version of the prometheus image (./telemetry/prometheus/Dockerfile) to handle the problem that the prom/prometheus image doesn't substitute the env variables inside the prometheus.yml config file

--- scripts/start_local_docker_environment.sh
+++ scripts/start_local_docker_environment.sh
@@ -3,7 +3,7 @@
 echo "Starting local replica"
 dfx start --clean --background
 
-echo "Removing previous local image"
+echo "Removing previous local websocket image"
 docker rmi local/ic-websocket-gateway
 
 # https://docs.docker.com/compose/environment-variables/set-environment-variables/#:~:text=doesnotexist/.env.dev-,You%20can%20use%20multiple,them%20in%20order.%20Later%20files%20can%20override%20variables%20from%20earlier%20files.,-%24%20docker%20compose
