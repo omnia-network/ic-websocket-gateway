a80b3f004211236b77a8d29c1e7b8e098c3b8d67
Lorenzo Ronzani <lorenzo.ronzani00@gmail.com>
2024-03-20 08:05:58 +0100

    refactor ⚙:  Removed .env override chance from start_local_docker_environment.sh

--- scripts/start_local_docker_environment.sh
+++ scripts/start_local_docker_environment.sh
@@ -8,4 +8,4 @@ docker rmi local/ic-websocket-gateway
 
 # https://docs.docker.com/compose/environment-variables/set-environment-variables/#:~:text=doesnotexist/.env.dev-,You%20can%20use%20multiple,them%20in%20order.%20Later%20files%20can%20override%20variables%20from%20earlier%20files.,-%24%20docker%20compose
 echo "Starting docker environment"
-docker compose -f docker-compose.yml -f docker-compose-local.yml --env-file .env --env-file .env.local up -d --build
+docker compose -f docker-compose.yml -f docker-compose-local.yml --env-file .env.local up -d --build
